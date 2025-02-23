8ca43a7f7216ed9e60392a440fbfe59e485a1900
Neeraj-singh21 <singh.nickey.ns@gmail.com>
2025-02-23 17:05:22 +0530

    Added Java programs

---
+++ JavaExercise/src/Java2.java
@@ -0,0 +1,18 @@
+public class Java2 {
+	public static void main(String Args[]) {
+		int a = 10 , b = 23;
+		
+		
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		//Typecasting 
+		System.out.println((double)a/b);
+		System.out.println((double)a%b);
+		System.out.println(a==b);
+		
+		
+
+	}
+}
